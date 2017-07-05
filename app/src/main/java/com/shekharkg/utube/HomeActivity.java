827204273c6ff03c8fbed6d5fe1a30b4a70e17e5
ShekharKG <shekhar114@gmail.com>
2017-07-05 22:41:36 +0530

    Added youtube player api and dataBinding enabled

--- app/src/main/java/com/shekharkg/utube/HomeActivity.java
+++ app/src/main/java/com/shekharkg/utube/HomeActivity.java
@@ -1,13 +1,22 @@
+/*
+ * Copyright (c)  2017 Shekhar Gupta. - All Rights Reserved
+ */
+
 package com.shekharkg.utube;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.shekharkg.utube.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+  private ActivityHomeBinding homeBinding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_home);
+    homeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home);
   }
 }
